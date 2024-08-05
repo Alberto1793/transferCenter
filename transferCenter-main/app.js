0fd0b8ed6469caf6ff3f2eebac88b1c717e75500
Alberto Nazario <77199306+Alberto1793@users.noreply.github.com>
2024-08-05 07:04:13 -0400

    Add files via upload

---
+++ transferCenter-main/app.js
@@ -0,0 +1,22 @@
+// USE FUNCTION TO MAKE BUTTONS WORK ON HOME PAGE
+// document.getElementById('openQgendaBtn').onclick = function () {
+//   window.location = 'qgenda.html';
+//   return false;
+// };
+
+document.getElementById('openExternalBtn').onclick = function () {
+  window.location = 'external.html';
+  return false;
+};
+
+document.getElementById('openFaqBtn').onclick = function () {
+  window.location = 'faq.html';
+
+  return false;
+};
+
+document.getElementById('openOncallBtn').onclick = function () {
+  window.location = 'oncall.html';
+
+  return false;
+};
