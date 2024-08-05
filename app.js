ff7a2ff3a8f25212472bc49f3fabab3df9393b0b
Alberto Nazario <77199306+Alberto1793@users.noreply.github.com>
2024-08-05 07:09:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // USE FUNCTION TO MAKE BUTTONS WORK ON HOME PAGE
-document.getElementById('openQgendaBtn').onclick = function () {
-  window.location = 'qgenda.html';
-  return false;
-};
+// document.getElementById('openQgendaBtn').onclick = function () {
+//   window.location = 'qgenda.html';
+//   return false;
+// };
 
 document.getElementById('openExternalBtn').onclick = function () {
   window.location = 'external.html';
@@ -11,6 +11,12 @@ document.getElementById('openExternalBtn').onclick = function () {
 
 document.getElementById('openFaqBtn').onclick = function () {
   window.location = 'faq.html';
+
+  return false;
+};
+
+document.getElementById('openOncallBtn').onclick = function () {
+  window.location = 'oncall.html';
 
   return false;
 };
